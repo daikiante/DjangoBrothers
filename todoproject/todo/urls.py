efff519cdc7e9f82ba376582a3dfbaef40696252
daikiante <daikiante@gmail.com>
2019-12-25 23:02:30 +0530

    2019/12/25

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import TodoList
+from .views import TodoList, ToDoDetail, TodoCreate
+from . import views
 
+
+app_name ='todo'
 
 urlpatterns = [
-    path('list/', TodoList.as_view()),
+    path('', views.index, name='index'),
+    path('list/', TodoList.as_view(), name='list'),
+    path('detail/<int:pk>', ToDoDetail.as_view(), name='detail'),
+    path('create/', TodoCreate.as_view(), name='create')
 ]
--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
+from django.urls import reverse_lazy
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+
 class TodoList(ListView):
     template_name = 'list.html'
     model = TodoModel
+
+
+class ToDoDetail(DetailView):
+    template_name = 'detail.html'
+    model = TodoModel
+
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'text', 'priority', 'duedate')
+    success_url = reverse_lazy('todo:list')
