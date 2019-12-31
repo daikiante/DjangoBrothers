39d0fb78fb60c0505c9644794ab8d4019f938cf3
daikiante <daikiante@gmail.com>
2019-12-31 11:09:30 +0530

    2019/12/31

--- sql_practice/sql/views.py
+++ sql_practice/sql/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Content
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'sql/index.html')
+    items = Content.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'sql/index.html', context)
--- sql_practice/sql_practice/settings.py
+++ sql_practice/sql_practice/settings.py
@@ -62,23 +62,20 @@ WSGI_APPLICATION = 'sql_practice.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sql_practice',
+        'NAME': 'Local_Instance',
         'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '8889',
+        'PASSWORD': 'daiki1214',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+
     }
 }
 
